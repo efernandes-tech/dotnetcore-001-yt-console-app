17d486513860cb4a33b41a5219c098b774c85863
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-08-30 10:05:58 -0300

    First version

---
+++ EF.Console/EF.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+
+namespace EF.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var build = new ConfigurationBuilder();
+
+            build.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(build.Build())
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Information");
+            Log.Logger.Warning("Warning");
+            Log.Logger.Error("Error");
+            Log.Logger.Fatal("Information");
+
+            System.Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotnetcore-001-yt-console-app
+
+## Dependências:
+- CommandLineParser: Executar a aplicação passando parâmetros de forma mais profissional.
+- Serilog: Logar informações na tela.
+    - Serilog.Extensions.Hosting: Necessário para subistituir o serlog padrão.
+    - Serilog.Settings.Configuration: Para configurar a partir do arquivo appsettings.
+    - Serilog.Sinks.Console: Para exibir os logs em tela.
+- Microsoft.Extensions.Configuration
+- Microsoft.Extensions.Hosting
+- Microsoft.Extensions.DependencyInjection
